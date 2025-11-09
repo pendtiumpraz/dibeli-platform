ba8ea028cf6c35b2cab252c7cc8f1df703adda34
Galih Prasetyo <pendtiumpraz@gmail.com>
2025-11-10 01:12:46 +0700

    Fix WhatsApp message encoding - remove emoji characters

--- src/templates/branded-cards-dynamic.js
+++ src/templates/branded-cards-dynamic.js
@@ -5,9 +5,13 @@ function orderProduct(productName, price) {
   const whatsappNumber = '{{store.whatsappNumber}}'
   const storeName = '{{store.name}}'
   
-  const message = `Halo *${storeName}*! 👋\n\nSaya tertarik dengan:\n\n📦 *Produk:* ${productName}\n💰 *Harga:* ${price}\n\n❓ Apakah masih tersedia?`
+  const message = 'Halo *' + storeName + '*!\n\n' +
+    'Saya tertarik dengan:\n\n' +
+    '*Produk:* ' + productName + '\n' +
+    '*Harga:* ' + price + '\n\n' +
+    'Apakah masih tersedia?'
   
-  const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`
+  const whatsappUrl = 'https://wa.me/' + whatsappNumber + '?text=' + encodeURIComponent(message)
   
   window.open(whatsappUrl, '_blank')
 }
